78b72e0d29fbeb3dda5f458fa8baa9e9650fe098
NaveedAfraz <naveedafraz123@gmail.com>
2025-05-04 22:37:01 +0530

    added UserModel

--- backend/identity-service/models/user.js
+++ backend/identity-service/models/user.js
@@ -1,4 +1,54 @@
 const mongooese = require("mongoose");
 const argon2 = require("argon2");
 
-const userScheme = new mongooese.Schema({});
+const userScheme = new mongooese.Schema(
+  {
+    Username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userScheme.pre("save", async function (next) {
+  if (this.isModified("Password")) {
+    try {
+      this.Password = await argon2.hash(this.Password);
+    } catch (error) {
+      next(error);
+    }
+  }
+  next();
+});
+
+userScheme.methods.comparePassword = async function (password) {
+  try {
+    return await argon2.verify(this.Password, password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongooese.model("User", userScheme);
+module.exports = User;
