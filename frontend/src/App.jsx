5bba9b761ab29369d6e4941fdf26b50aaf541a2e
NaveedAfraz <naveedafraz123@gmail.com>
2025-05-29 22:23:11 +0530

    added analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import { useEffect } from "react";
 import useCurrentUser from "./hooks/auth/useCurrentuser";
 import { useNavigate, useLocation } from "react-router";
 import { Home } from "./pages/home";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   const queryClient = useQueryClient();
   const { currentUser, isError, error, isLoading, refetch } = useCurrentUser();
@@ -32,10 +33,17 @@ function App() {
       navigate("/auth/login");
     }
   }, [isLoading, isError]);
-  if (isLoading) return <div>Loadicng...</div>;
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    );
   return (
     <Routes>
+      <Analytics />
       <Route path="/" element={<Home />}>
+
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/pin/:id" element={<PostPage />} />
         <Route path="/Explore" element={<Explore />} />
